d6c24c8bfab80027fceb52e4430a7d79904d50fe
PedroDelmiro13 <pdelmiro1309@gmail.com>
2025-07-22 13:46:32 -0300

    iniciando

---
+++ taskmatch/src/main/java/com/taskmatch/taskmatch/TaskmatchApplication.java
@@ -0,0 +1,13 @@
+package com.taskmatch.taskmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmatchApplication.class, args);
+	}
+
+}
---
+++ taskmatch/src/test/java/com/taskmatch/taskmatch/TaskmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmatch.taskmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
